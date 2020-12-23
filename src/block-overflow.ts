910655e6a3b2d2f801540ef67d6475df6a938fa1
Josh Froelich <joshua.froelich@gmail.com>
2020-12-23 11:46:04 -0800

    cleanup some fix logic in block-overflow

--- src/block-overflow.ts
+++ src/block-overflow.ts
@@ -10,7 +10,7 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
   // TODO: revise so that this is const, if we want to work on altered state that should be a
   // different variable
 
-  let text = context.code.lines[context.line - 1];
+  const text = context.code.lines[context.line - 1];
 
   // Detect if we are transitioning into a markdown fenced section or out of a mark down fenced
   // section.
@@ -20,7 +20,9 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
   // variable that is reused in several places, the captures indent level and whether this is a
   // asterisk block and where the content starts and where the asterisk is located
 
-  if (text.trimStart().startsWith('* ```')) {
+  const textTrimmedStart = text.trimStart();
+
+  if (textTrimmedStart.startsWith('* ```')) {
     context.fenced = !context.fenced;
   }
 
@@ -30,20 +32,28 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
     return;
   }
 
+  // If the entire text of the line (not just the part of the line that is the comment) is less than
+  // the threshold then the line does not overflow.
+
   if (text.length <= context.max_line_length) {
     return;
   }
 
-  text = text.trimStart();
+  // Do not treat tslint directives as overflowing. tslint directives are expressed on a single line
+  // of the block comment (at least that is what I understand). Since we could be on any line of the
+  // comment, we want to check if we are on the first line of the comment. We tolerate leading
+  // whitespace before the comment.
+  // TODO: what about whitespace between the slash-star and the word tslint?
 
-  // Do not treat tslint directives as overflowing
-
-  if (context.line === context.comment.loc.start.line && text.startsWith('/* tslint:')) {
+  if (context.line === context.comment.loc.start.line &&
+    textTrimmedStart.startsWith('/* tslint:')) {
     return;
   }
 
   // Compute the position of the start of the current line in the whole file. The +1 is the length
-  // of the line break (which might be wrong right now).
+  // of the line break (which might be wrong right now). This is computed starting from the start of
+  // the comment. We could be on the first line of the comment or some later line. The loop is not
+  // entered when we are on the first line.
 
   let lineRangeStart = context.comment.range[0];
   for (let line = context.comment.loc.start.line; line < context.line; line++) {
@@ -54,8 +64,8 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
   // an asterisk.
 
   let edge = -1;
-  if (text.startsWith('* ')) {
-    edge = text.slice(2).lastIndexOf(' ', context.max_line_length - 2);
+  if (textTrimmedStart.startsWith('* ')) {
+    edge = textTrimmedStart.slice(2).lastIndexOf(' ', context.max_line_length - 2);
 
     // the slice wreaks some havoc on the offset
     if (edge + 3 > context.max_line_length) {
@@ -64,11 +74,25 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
       edge = edge + 3;
     }
   } else {
-    // we trimmed left. we are starting with * or whatever is first text.
-    edge = text.lastIndexOf(' ', context.max_line_length);
+    edge = textTrimmedStart.lastIndexOf(' ', context.max_line_length);
   }
 
-  const report: eslint.Rule.ReportDescriptor = {
+  // Compute the range of the text after which we will insert some new text.
+
+  let insertAfterRange: eslint.AST.Range;
+  if (edge === -1) {
+    insertAfterRange = [0, lineRangeStart + context.max_line_length];
+  } else {
+    insertAfterRange = [0, lineRangeStart + edge];
+  }
+
+  // Build the text to insert.
+
+  const firstOverflowingCharacter = edge === -1 ?
+    text.charAt(context.max_line_length) : text.charAt(edge);
+  const textToInsert = firstOverflowingCharacter === ' ' ? '\n*' : '\n* ';
+
+  return <eslint.Rule.ReportDescriptor>{
     node: context.node,
     loc: context.comment.loc,
     messageId: 'overflow',
@@ -77,27 +101,7 @@ export function createBlockCommentLineOverflowReport(context: CommentContext) {
       max_length: `${context.max_line_length}`
     },
     fix: function (fixer) {
-      const text = context.code.lines[context.line - 1];
-      if (edge === -1) {
-        const firstOverflowingCharacter = text.charAt(context.max_line_length);
-        const insertedText = firstOverflowingCharacter === ' ' ? '\n*' : '\n* ';
-        const range: eslint.AST.Range = [
-          0,
-          lineRangeStart + context.max_line_length
-        ];
-
-        return fixer.insertTextAfterRange(range, insertedText);
-      } else {
-        const firstOverflowingCharacter = text.charAt(edge);
-        const insertedText = firstOverflowingCharacter === ' ' ? '\n*' : '\n* ';
-        const range: eslint.AST.Range = [
-          0,
-          lineRangeStart + edge
-        ];
-        return fixer.insertTextAfterRange(range, insertedText);
-      }
+      return fixer.insertTextAfterRange(insertAfterRange, textToInsert);
     }
   };
-
-  return report;
 }
