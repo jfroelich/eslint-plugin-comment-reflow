d97fc163bc217dec8f100b18a052bcf03c0e0c88
Josh Froelich <joshua.froelich@gmail.com>
2020-12-28 00:07:26 -0800

    improve CommentLine docs

--- src/comment-length-rule/comment-line.ts
+++ src/comment-length-rule/comment-line.ts
@@ -2,21 +2,31 @@
  * Describes a line of a comment
  */
 export interface CommentLine {
-  /** The ESLint line index, which is 1-based. */
+  /**
+   * The ESLint line index, which is 1-based. This should not be confused with some kind of index
+   * into an array of lines for a comment. This is the global index for the entire file.
+   */
   index: number;
 
-  /** Whitespace characters leading up to the open or prefix. */
+  /**
+   * Whitespace characters leading up to the open region. For a block comment, it is assumed that
+   * block comments that follow another kind of token on the same line are never analyzed, so this
+   * means that this is whitespace starting from the first character in the line.
+   */
   lead_whitespace: string;
 
   /**
-   * The characters that start the comment. For block comments this is only set on the first line.
-   * This does not include the whitespace preceding or following the syntax.
+   * The syntactical characters that start the comment. For block comments this is only set on the
+   * first line and for all other lines is an empty string. This does not include the whitespace
+   * preceding or following the characters. For javadoc-formatted block comments this does not
+   * include the second asterisk. For triple slash line comments, this does not include the third
+   * slash.
    */
   open: string;
 
   /**
-   * The characters that end the comment. For block comments this is only set on the last line. For
-   * single line comments this is an empty string. This does not include the whitespace preceding or
+   * The characters that end the comment. For block comments this is set on the last line. In all
+   * other situations this is an empty string. This does not include the whitespace preceding or
    * following the star slash.
    */
   close: string;
@@ -26,7 +36,8 @@ export interface CommentLine {
    * because of how ESLint parses the lines. The text may include characters that are not a part of
    * the comment because the value is derived from a value in ESLint's line array which is computed
    * separately from its comments array. The text includes the comment syntax like the forward
-   * slashes.
+   * slashes. Because it is assumed that block comments that follow another kind of token on the
+   * same line are not analyzed, the text starts from the first character on the line.
    */
   text: string;
 
@@ -40,19 +51,20 @@ export interface CommentLine {
   prefix: string;
 
   /**
-   * The text of the comment line excluding leading and trailing whitespace. Overlaps with markup.
+   * The text of the comment line between its prefix and suffix.
    */
   content: string;
 
   /**
-   * Overlaps with content. The content is parsed for semantic information such as markdown or
-   * jsdoc.
+   * Represents the occassional initial markup that appears at the start of the line's content such
+   * as a jsdoc tag or some markdown. Markup does not precede the content like the prefix; it
+   * overlaps.
    */
   markup: string;
 
   /**
-   * Overlaps with content. Represents the whitespace immediately following the markup token up to
-   * the first non-space character (exclusive), if any markup was found.
+   * Represents the whitespace immediately following the markup token up to the first non-space
+   * character (exclusive). Overlaps with content.
    */
   markup_space: string;
 
