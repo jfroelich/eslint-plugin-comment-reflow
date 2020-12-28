8f7b67e63f936c54df9e4ec6bd08ee94848baba7
Josh Froelich <joshua.froelich@gmail.com>
2020-12-27 23:47:23 -0800

    rework internals of parseLine
    since the bulk of the logic varies according to one of 5 situations, and not which part of the comment is being parsed, structure the logic around the 5 situations

--- src/comment-length-rule/parse-line.ts
+++ src/comment-length-rule/parse-line.ts
@@ -2,98 +2,75 @@ import eslint from 'eslint';
 import estree from 'estree';
 import { CommentLine } from './comment-line';
 
-/**
- * @todo since our primary logic branches on one of five situations, we should organize the code
- * around those five situations, not based on which part of the line we are parsing.
- */
 export function parseLine(code: eslint.SourceCode, comment: estree.Comment, line: number) {
   const output = <CommentLine>{};
   output.index = line;
   output.text = code.lines[line - 1];
 
-  // Parse the leading whitespace. For a single line comment, or the first line of a block comment,
-  // this is all whitespace leading up to the first slash. For other lines of a block comment, this
-  // is all whitespace leading up to the first non-whitespace character.
-
   const textTrimmedStart = output.text.trimStart();
   output.lead_whitespace = output.text.slice(0, output.text.length - textTrimmedStart.length);
 
-  // Set the opening and closing text
-
-  if (comment.type === 'Block') {
-    if (line === comment.loc.start.line && line === comment.loc.end.line) {
-      output.open = '/*';
-      output.close = '*/';
-    } else if (line === comment.loc.start.line) {
-      output.open = '/*';
-      output.close = '';
-    } else if (line === comment.loc.end.line) {
-      output.open = '';
-      output.close = '*/';
-    } else {
-      output.open = '';
-      output.close = '';
-    }
-  } else if (comment.type === 'Line') {
+  if (comment.type === 'Line') {
     output.open = '//';
     output.close = '';
-  }
-
-  // Parse the prefix. For line comments this includes the two forward slashes and any whitespace
-  // preceding the content. For the first line of a block comment this includes the forward slash,
-  // asterisk(s), and any whitespace preceding the content. For other lines, this includes the
-  // leading asterisk if one is present and any whitespace preceding the content.
-
-  if (comment.type === 'Block') {
-    if (line === comment.loc.start.line && line === comment.loc.end.line) {
-      const haystack = output.text.slice(output.lead_whitespace.length + output.open.length,
-        comment.loc.end.column - output.close.length);
-      const matches = /^\**\s*/.exec(haystack);
-      output.prefix = matches ? matches[0] : '';
-    } else if (line === comment.loc.start.line) {
-      const haystack = output.text.slice(output.lead_whitespace.length + output.open.length);
-      const matches = /^\*+\s*/.exec(haystack);
-      output.prefix = matches ? matches[0] : '';
-    } else if (line === comment.loc.end.line) {
-      const haystack = output.text.slice(output.lead_whitespace.length,
-        comment.loc.end.column - output.close.length);
-      const matches = /^\*\s+/.exec(haystack);
-      output.prefix = matches ? matches[0] : '';
-    } else {
-      const matches = /^\*\s+/.exec(textTrimmedStart);
-      output.prefix = matches ? matches[0] : '';
-    }
-  } else if (comment.type === 'Line') {
-    const text = output.text.slice(output.lead_whitespace.length + output.open.length);
-    const textTrimmedStart = text.trimStart();
-    const whitespacePrefixLength = text.length - textTrimmedStart.length;
+    const beforePrefix = output.text.slice(output.lead_whitespace.length + output.open.length);
+    const beforePrefixTrimmed = beforePrefix.trimStart();
+    const beforePrefixSpaceLen = beforePrefix.length - beforePrefixTrimmed.length;
     output.prefix = output.text.slice(output.lead_whitespace.length + output.open.length,
-      output.lead_whitespace.length + output.open.length + whitespacePrefixLength);
-  }
-
-  // Parse the content. This is all content following the prefix and preceding the suffix. The
-  // content does not include leading or trailing whitespace.
-
-  if (comment.type === 'Block') {
-    if (line === comment.loc.start.line && line === comment.loc.end.line) {
-      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length, comment.loc.end.column - output.close.length).trimEnd();
-    } else if (line === comment.loc.start.line) {
-      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length).trimEnd();
-    } else if (line === comment.loc.end.line) {
-      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length, comment.loc.end.column - output.close.length).trimEnd();
-    } else {
-      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length).trimEnd();
-    }
-  } else if (comment.type === 'Line') {
+      output.lead_whitespace.length + output.open.length + beforePrefixSpaceLen);
     output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
       output.prefix.length).trimEnd();
+    output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
+      output.prefix.length + output.content.length);
+  } else if (comment.type === 'Block') {
+    if (line === comment.loc.start.line && line === comment.loc.end.line) {
+      output.open = '/*';
+      output.close = '*/';
+      const prefixHaystack = output.text.slice(output.lead_whitespace.length + output.open.length,
+        comment.loc.end.column - output.close.length);
+      const prefixMatch = /^\**\s*/.exec(prefixHaystack);
+      output.prefix = prefixMatch ? prefixMatch[0] : '';
+      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length, comment.loc.end.column - output.close.length).trimEnd();
+      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length + output.content.length, comment.loc.end.column - output.close.length);
+    } else if (line === comment.loc.start.line) {
+      output.open = '/*';
+      output.close = '';
+      const prefixHaystack = output.text.slice(output.lead_whitespace.length + output.open.length);
+      const prefixMatch = /^\*+\s*/.exec(prefixHaystack);
+      output.prefix = prefixMatch ? prefixMatch[0] : '';
+      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length).trimEnd();
+      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length + output.content.length);
+    } else if (line === comment.loc.end.line) {
+      output.open = '';
+      output.close = '*/';
+      const prefixHaystack = output.text.slice(output.lead_whitespace.length,
+        comment.loc.end.column - output.close.length);
+      const prefixMatch = /^\*\s+/.exec(prefixHaystack);
+      output.prefix = prefixMatch ? prefixMatch[0] : '';
+      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length, comment.loc.end.column - output.close.length).trimEnd();
+      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length + output.content.length, comment.loc.end.column - output.close.length);
+    } else {
+      output.open = '';
+      output.close = '';
+      const prefixMatch = /^\*\s+/.exec(textTrimmedStart);
+      output.prefix = prefixMatch ? prefixMatch[0] : '';
+      output.content = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length).trimEnd();
+      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
+        output.prefix.length + output.content.length);
+    }
+  } else {
+    // eslint for some reason if forgetting about its own shebang type in the AST
+    throw new TypeError(`Unexpected comment type "${<string>comment.type}"`);
   }
 
-  // Parse markup such as markdown and jsdoc.
+  // Re-parse the content for markup such as markdown and jsdoc.
 
   if (output.content.length) {
     const matches = /^([*-]|\d+\.|@[a-zA-Z]+)(\s+)/.exec(output.content);
@@ -107,30 +84,6 @@ export function parseLine(code: eslint.SourceCode, comment: estree.Comment, line
   } else {
     output.markup = '';
     output.markup_space = '';
-  }
-
-  // Parse the suffix. For a single line comment this is all trailing whitespace after the last
-  // non-whitespace character. For the last line of a block comment this is all whitespace following
-  // the content and the comment end syntax. For other lines of a block comment this is all
-  // whitespace following the content. The suffix does not include a line break.
-
-  if (comment.type === 'Block') {
-    if (line === comment.loc.start.line && line === comment.loc.end.line) {
-      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length + output.content.length, comment.loc.end.column - output.close.length);
-    } else if (line === comment.loc.start.line) {
-      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length + output.content.length);
-    } else if (line === comment.loc.end.line) {
-      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length + output.content.length, comment.loc.end.column - output.close.length);
-    } else {
-      output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
-        output.prefix.length + output.content.length);
-    }
-  } else if (comment.type === 'Line') {
-    output.suffix = output.text.slice(output.lead_whitespace.length + output.open.length +
-      output.prefix.length + output.content.length);
   }
 
   return output;
