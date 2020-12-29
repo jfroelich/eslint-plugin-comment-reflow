502984ef7878169cb152ca01e1e6f87bd0afb6aa
Josh Froelich <joshua.froelich@gmail.com>
2020-12-28 17:45:03 -0800

    merge block-comment and line-comment into index

--- src/comment-length-rule/index.ts
+++ src/comment-length-rule/index.ts
@@ -1,10 +1,12 @@
 import assert from 'assert';
 import eslint from 'eslint';
 import estree from 'estree';
-import { checkBlockComment } from './block-comment';
+import { checkBlockOverflow } from './block-overflow';
+import { checkBlockUnderflow } from './block-underflow';
 import { CommentContext } from './comment-context';
 import { CommentLine } from './comment-line';
-import { checkLineComment } from './line-comment';
+import { checkLineOverflow } from './line-overflow';
+import { checkLineUnderflow } from './line-underflow';
 import { parseLine } from './parse-line';
 
 export const commentLengthRule: eslint.Rule.RuleModule = {
@@ -64,6 +66,63 @@ function analyzeProgram(context: eslint.Rule.RuleContext, node: estree.Node) {
       // Ignore shebang. A shebang should only be the first comment in a file. I am unclear on
       // whether eslint even reveals it via getAllComments. In any event, we never want to check it
       // for split/merge. So we support shebang comments by properly ignoring them.
+    }
+  }
+}
+
+function checkBlockComment(context: CommentContext, comment: estree.Comment) {
+  const previousToken = context.code.getTokenBefore(comment, { includeComments: true });
+  if (previousToken && previousToken.loc.end.line === comment.loc.start.line) {
+    return;
+  }
+
+  const nextToken = context.code.getTokenAfter(comment, { includeComments: true });
+  if (nextToken && comment.loc.end.line === nextToken.loc.start.line) {
+    return;
+  }
+
+  context.in_md_fence = false;
+  context.in_jsdoc_example = false;
+
+  for (let loc = comment.loc, line = loc.start.line, previousLine: CommentLine;
+    line <= loc.end.line; line++) {
+    const currentLine = parseLine(context.code, comment, line);
+
+    let report = checkBlockOverflow(context, comment, currentLine);
+    if (report) {
+      return report;
+    }
+
+    if (previousLine) {
+      report = checkBlockUnderflow(context, previousLine, currentLine);
+      if (report) {
+        return report;
+      }
+    }
+
+    previousLine = currentLine;
+  }
+}
+
+function checkLineComment(context: CommentContext, comment: estree.Comment,
+  previousLine: CommentLine, currentLine: CommentLine) {
+  // Ignore trailing line comments. Eventually this can be supported but doing so complicates the
+  // logic so for now just ignore.
+
+  const previousToken = context.code.getTokenBefore(comment, { includeComments: true });
+  if (previousToken && previousToken.loc.end.line === comment.loc.start.line) {
+    return;
+  }
+
+  const overflowReport = checkLineOverflow(context, currentLine);
+  if (overflowReport) {
+    return overflowReport;
+  }
+
+  if (previousLine) {
+    const underflowReport = checkLineUnderflow(context, previousLine, currentLine);
+    if (underflowReport) {
+      return underflowReport;
     }
   }
 }
