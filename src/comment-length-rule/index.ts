8843f5123fc35ae25dfcbb427ccd5d28b45c6a47
Josh Froelich <joshua.froelich@gmail.com>
2021-01-10 17:47:35 -0800

    impl findCommentGroups

--- src/comment-length-rule/index.ts
+++ src/comment-length-rule/index.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import eslint from 'eslint';
 import estree from 'estree';
-import { CommentLine, CommentLineGroup, parseLine, sniffLineBreakStyle } from './util';
+import { CommentLine, CommentLineGroup, parseLine } from './util';
 
 export default <eslint.Rule.RuleModule>{
   meta: {
@@ -17,13 +17,13 @@ export default <eslint.Rule.RuleModule>{
 
 function createCommentLengthRule(context: eslint.Rule.RuleContext) {
   return {
-    Program: function(node: estree.Node) {
-      return analyzeProgram(context, node);
+    Program: function(/*node: estree.Node*/) {
+      return analyzeProgram(context/*, node*/);
     }
   };
 }
 
-function analyzeProgram(ruleContext: eslint.Rule.RuleContext, node: estree.Node) {
+function analyzeProgram(ruleContext: eslint.Rule.RuleContext/*, node: estree.Node*/) {
   let maxLineLength = 80;
   if (ruleContext.options && ruleContext.options.length) {
     maxLineLength = <number>ruleContext.options[0];
@@ -31,89 +31,35 @@ function analyzeProgram(ruleContext: eslint.Rule.RuleContext, node: estree.Node)
 
   assert(Number.isInteger(maxLineLength), 'Invalid option for maximum line length');
 
-  const lineBreakStyle = sniffLineBreakStyle(ruleContext);
   const code = ruleContext.getSourceCode();
   const comments = code.getAllComments();
-  let lines: CommentLine[] = [];
-  const reports: eslint.Rule.ReportDescriptor[] = [];
 
-  const isolatedComments = findIsolatedComments(ruleContext, comments);
+  const isolatedComments = findCandidateComments(ruleContext, comments);
+  const groups = findCommentGroups(ruleContext, isolatedComments);
 
-  for (const comment of isolatedComments) {
-    if (comment.type === 'Block') {
-      if (lines.length) {
-        const group = <CommentLineGroup>{
-          type: 'line',
-          node,
-          code,
-          line_break: lineBreakStyle,
-          max_line_length: maxLineLength,
-          lines
-        };
-        reports.push(analyzeGroup(group));
-        lines = [];
-      }
+  console.log(groups);
 
-      for (let line = comment.loc.start.line; line <= comment.loc.end.line; line++) {
-        const currentLine = parseLine(code, comment, line);
-        lines.push(currentLine);
-      }
+  // TODO: analyze the groups and generate reports
+  // const lineBreakStyle = sniffLineBreakStyle(ruleContext);
 
-      const group = <CommentLineGroup>{
-        type: 'block',
-        node,
-        code,
-        line_break: lineBreakStyle,
-        max_line_length: maxLineLength,
-        lines
-      };
-      reports.push(analyzeGroup(group));
-      lines = [];
-    } else if (comment.type === 'Line') {
-      const currentLine = parseLine(code, comment, comment.loc.start.line);
-      if (lines.length === 0 || lines[lines.length -1].index + 1 === comment.loc.start.line) {
-        lines.push(currentLine);
-      } else {
-        const group = <CommentLineGroup>{
-          type: 'line',
-          node,
-          code,
-          line_break: lineBreakStyle,
-          max_line_length: maxLineLength,
-          lines
-        };
-        reports.push(analyzeGroup(group));
-        lines = [currentLine];
-      }
-    } else {
-      // ignore shebang, and since it should only be first line, lines array is empty
-    }
-  }
-
-  if (lines.length) {
-    const group = <CommentLineGroup>{
-      type: 'line',
-      node,
-      code,
-      line_break: lineBreakStyle,
-      max_line_length: maxLineLength,
-      lines
-    };
-    reports.push(analyzeGroup(group));
-  }
-
-  console.log('Found %d report descriptors', reports.length);
-
-  // todo: iterate over the reports and report each one
+  // TODO: iterate over the reports and report each one
+  //   const reports: eslint.Rule.ReportDescriptor[] = [];
 }
 
 /**
  * Returns an array of comments that do not share lines with non-comment tokens.
  */
-function findIsolatedComments(context: eslint.Rule.RuleContext, comments: estree.Comment[]) {
+function findCandidateComments(context: eslint.Rule.RuleContext, comments: estree.Comment[]) {
   const code = context.getSourceCode();
 
   return comments.filter(comment => {
+    // ignore shebang
+    if (comment.type !== 'Block' && comment.type !== 'Line') {
+      return false;
+    }
+
+    // ignore comments where another token precedes on same line
+
     const previousToken = code.getTokenBefore(comment, { includeComments: true });
     if (previousToken && previousToken.loc.end.line === comment.loc.start.line) {
       return false;
@@ -132,12 +78,57 @@ function findIsolatedComments(context: eslint.Rule.RuleContext, comments: estree
   });
 }
 
-function analyzeGroup(group: CommentLineGroup) {
-  if (!group) {
-    return;
+/**
+ * Scans the incoming array and aggregates comments into groups. Block comments represent 1 group.
+ * Sequential line comments represent 1 group.
+ *
+ * This assumes that comments are the only token on the line.
+ *
+ * @todo maybe eagerly parsing is bad if we do not always use all the lines? i like maintaining the
+ * link to the comment of the line though.
+ */
+export function findCommentGroups(context: eslint.Rule.RuleContext, comments: estree.Comment[]) {
+  const code = context.getSourceCode();
+  const groups: Partial<CommentLineGroup>[] = [];
+  let lines: CommentLine[] = [];
+
+  for (const comment of comments) {
+    if (comment.type === 'Block') {
+      if (lines.length) {
+        groups.push({ type: 'line', lines });
+        lines = [];
+      }
+
+      for (let line = comment.loc.start.line; line <= comment.loc.end.line; line++) {
+        const currentLine = parseLine(code, comment, line);
+        lines.push(currentLine);
+      }
+
+      groups.push({ type: 'block', lines });
+      lines = [];
+    } else if (comment.type === 'Line') {
+      const currentLine = parseLine(code, comment, comment.loc.start.line);
+
+      // when there is no previous line comment, append to buffer. when the current line is
+      // immediately subsequent to the previous line, append to buffer. otherwise, there are
+      // distinct groups of line comments, so flush the current buffer and start anew.
+      if (lines.length === 0 || lines[lines.length -1].index + 1 === comment.loc.start.line) {
+        lines.push(currentLine);
+      } else {
+        groups.push({ type: 'line', lines });
+        lines = [currentLine];
+      }
+    } else {
+      // ignore shebang
+    }
   }
 
-  console.log('got group with type', group.type);
+  // Due to the how the iteration is structured, we have to account for the final buffer of lines at
+  // the end.
 
-  return <eslint.Rule.ReportDescriptor>null;
+  if (lines.length) {
+    groups.push({ type: 'line', lines });
+  }
+
+  return groups;
 }
