c735743df1103197a035e5890f1922298b5f2904
Josh Froelich <joshua.froelich@gmail.com>
2021-05-07 18:42:39 -0700

    fleshing out more of comment-length-rule

--- src/comment-length-rule.ts
+++ src/comment-length-rule.ts
@@ -1,12 +1,19 @@
 import eslint from 'eslint';
 import estree from 'estree';
 
+interface ScanState {
+  max_line_length: number;
+  line_break_chars: string;
+  program: estree.Program;
+  context: eslint.Rule.RuleContext;
+}
+
 export default <eslint.Rule.RuleModule>{
   meta: {
     type: 'layout',
     fixable: 'whitespace',
     messages: {
-      reflow: 'Comment line {{line}} needs reflow.'
+      reflow: 'Comment starting on line {{line}} needs reflow.'
     }
   },
   create: function (context: eslint.Rule.RuleContext) {
@@ -17,20 +24,17 @@ export default <eslint.Rule.RuleModule>{
 };
 
 function scan(program: estree.Program, context: eslint.Rule.RuleContext) {
-  const maxLineLength = context.options?.length && Number.isInteger(context.options[0]) ?
+  const state = <ScanState>{};
+  state.program = program;
+  state.context = context;
+  state.max_line_length = context.options?.length && Number.isInteger(context.options[0]) ?
     <number>context.options[0] : 80;
-  const lineBreakChars = findLineBreakChars(context);
+  state.line_break_chars = findLineBreakChars(context);
 
   const sourceCode = context.getSourceCode();
   const comments = sourceCode.getAllComments();
 
-  // Process the comments sequentially. We start by accumulating the comments into a small buffer
-  // that either consists of a single block comment or one or more line comments. We process the
-  // buffer each time we detect a non-contiguous comment or type change. We process whole buffers
-  // instead of individual lines because of strange eslint issues with generating many adjacent
-  // sequential fixes and to perform less rewriting. Any split can trigger the need to reflow again
-  // due to newly introduced underflow if changes are done per line, which is what an earlier
-  // version did, but it did not work so well.
+  // Sequentially buffer the comments and then process each buffer as it ends.
 
   let buffer: estree.Comment[] = [];
 
@@ -38,15 +42,13 @@ function scan(program: estree.Program, context: eslint.Rule.RuleContext) {
     const comment = comments[commentIndex];
 
     if (comment.type === 'Block') {
-      if (buffer.length) {
-        processCommentBuffer(buffer, program, maxLineLength, lineBreakChars);
-        buffer = [];
-      }
+      processCommentBuffer(buffer, state);
+      buffer = [];
 
-      const beforeToken = sourceCode.getTokenBefore(comment, { includeComments: true });
-      const afterToken = sourceCode.getTokenAfter(comment, { includeComments: true });
-      if ((!beforeToken || beforeToken.loc.end.line < comment.loc.start.line) &&
-        (!afterToken || afterToken.loc.start.line > comment.loc.end.line)) {
+      const before = sourceCode.getTokenBefore(comment, { includeComments: true });
+      const after = sourceCode.getTokenAfter(comment, { includeComments: true });
+      if ((!before || before.loc.end.line < comment.loc.start.line) &&
+        (!after || after.loc.start.line > comment.loc.end.line)) {
         buffer.push(comment);
       }
     } else if (comment.type === 'Line') {
@@ -56,59 +58,44 @@ function scan(program: estree.Program, context: eslint.Rule.RuleContext) {
           buffer[buffer.length - 1].loc.start.line === comment.loc.start.line - 1) {
           buffer.push(comment);
         } else {
-          if (buffer.length) {
-            processCommentBuffer(buffer, program, maxLineLength, lineBreakChars);
-            buffer = [];
-          }
-
+          processCommentBuffer(buffer, state);
+          buffer = [];
           buffer.push(comment);
         }
       } else {
-        // the comment cannot be processed.
-        if (buffer.length) {
-          processCommentBuffer(buffer, program, maxLineLength, lineBreakChars);
-          buffer = [];
-        }
-      }
-    } else {
-      // other comment types count as buffer breaks
-      if (buffer.length) {
-        processCommentBuffer(buffer, program, maxLineLength, lineBreakChars);
+        processCommentBuffer(buffer, state);
         buffer = [];
       }
+    } else {
+      processCommentBuffer(buffer, state);
+      buffer = [];
     }
   }
 
-  // We must take care to process the final buffer.
-
-  if (buffer.length) {
-    processCommentBuffer(buffer, program, maxLineLength, lineBreakChars);
-  }
+  processCommentBuffer(buffer, state);
 
   // Unlike before, we do not gather reports and then generate all the reports. We instead generate
   // reports online, as we detect them.
 }
 
-function processCommentBuffer(buffer: estree.Comment[], program: estree.Program,
-  maxLineLength: number, lineBreakChars: string) {
-  // TEMP: these are not yet in use but will be
-  console.log('program:', typeof program);
-  console.log('max line length:', maxLineLength);
-  console.log('line break chars:', lineBreakChars);
+function processCommentBuffer(buffer: estree.Comment[], state: ScanState) {
+  if (buffer.length === 0) {
+    return;
+  }
 
-  // TEMP: just previewing what the buffer looks like in development
-  console.log('%s(s):', buffer[0].type, buffer.map(comment => comment.value.trim()).join(','));
+  // TODO: iterate over the lines of the buffer. for a line, decide to split a line into two lines
+  // or merge two lines into one. keep in mind we want to only parse as needed so as to minimize
+  // parsing. i think we do something like mutate the replacement text, and if any mutation done,
+  // then we create a report. the problem i am currently wrestling with is how to store the
+  // replacement text, and how to "compare to previous". maybe we maintain an array of visited
+  // lines of text that will be merged at the end to compose the text. then we can also capture the
+  // parsed info per line in that array, and whether that line was "dirtied".
 
-  // TODO: we get a buffer consisting of either one block comment or one or more line comments.
-  // the next step is to process the comment lines. we are dealing with either all the lines of
-  // one block comment, or each line of different line comments.
-  // it is quite possible we actually want to be working with a buffer of lines here instead of
-  // comments but we lose out on a lot of comment properties this way.
-
-  // the gist of the algorithm is this: we parse a line. if it overflows, we merge it into the
-  // next line. if it underflows, we merge the next line into it. we somehow build some
-  // replacement text. if at least one change was made, we mark the whole comment as flawed, and
-  // report a fix that replaces the whole comment with the replacement text.
+  for (let line = buffer[0].loc.start.line; line <= buffer[buffer.length - 1].loc.end.line;
+    line++) {
+    const lineText = state.context.getSourceCode().lines[line - 1];
+    console.log('Line: %d "%s"', line - 1, lineText);
+  }
 }
 
 function findLineBreakChars(context: eslint.Rule.RuleContext) {
